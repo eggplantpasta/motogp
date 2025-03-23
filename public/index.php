fbb71826781fff61d639ffdc7e2abb360e5d04f1
Brett Roper <roper.brett@gmail.com>
2025-03-23 15:32:17 +0800

    Split off page partials

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'bootstrap.php';
+include 'partials/page_top.html';
+?>
+<h2>Welcome MotoGP bidding 2025</h2>
+<figure>
+  <img
+    src="/images/DannyPed.png"
+    alt="Danny Pedroza, 2017 Australian MotoGP at Phillip Island. Photo by Noisy." />
+  <figcaption>Zoooomm..</figcaption>
+</figure>
+<h2>Riders and Events</h2>
+<ul>
+  <li><a href="riders.php">Riders</a></li>
+  <li><a href="events.php">Events</a></li>
+</ul>
+
+<?php
+include 'partials/page_bottom.html';
+?>
