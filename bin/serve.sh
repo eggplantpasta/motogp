c8daf41733814dd9513ccbf43a6af55a792d3e4e
Brett Roper <roper.brett@gmail.com>
2025-04-16 08:25:46 +0800

    Serve new ini file

--- bin/serve.sh
+++ bin/serve.sh
@@ -3,8 +3,9 @@
 # Set the PHP_ROOT as the root level of the git repo
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
+# open the default browser to the local server
 xdg-open http://localhost:8080
 
 # Start the server
-php -S localhost:8080 -c ${ROOT_DIR}/config/php.ini -t ${ROOT_DIR}/public
+php -S localhost:8080 -c ${ROOT_DIR}/public/.user.ini -t ${ROOT_DIR}/public
 
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,4 +4,3 @@ define('ROOT_DIR', dirname(__DIR__));
 
 require_once ROOT_DIR . '/src/autoloader.php';
 require_once ROOT_DIR . '/config/config.php';
-
