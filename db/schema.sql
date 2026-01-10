c06a5b6c8ac63b561ea60b450afde5581dafe91d
Brett Roper <roper.brett@gmail.com>
2026-01-10 10:26:27 +0800

    Add audit column

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,38 @@
-CREATE TABLE user (
-  user_id integer PRIMARY KEY,
-  username varchar(255),
-  password varchar(255),
-  email varchar(255),
-  balance integer
+create table if not exists users (
+    user_id integer primary key,
+    username text not null,
+    password text not null,
+    email text not null,
+    admin integer not null default 0,
+    balance integer not null default 20,
+    created_at datetime not null default current_timestamp
 );
 
-CREATE TABLE riders (
-  rider_id integer PRIMARY KEY,
+create table if not exists  riders (
+  rider_id integer primary key,
   name varchar(255),
   team varchar(255),
   link varchar(255),
-  active boolean
+  active boolean,
+  created_at datetime not null default current_timestamp
 );
 
-CREATE TABLE events (
-  event_id integer PRIMARY KEY,
+create table if not exists  events (
+  event_id integer primary key,
   start_date date,
   name varchar(255),
   circuit varchar(255),
   flag varchar(255),
-  link varchar(255)
+  link varchar(255),
+  created_at datetime not null default current_timestamp
 );
 
-CREATE TABLE bids (
-  bid_id integer PRIMARY KEY,
+create table if not exists bids (
+  bid_id integer primary key,
   user_id integer,
   rider_id integer,
   event_id integer,
   bid_number integer,
-  amount integer
+  amount integer,
+  created_at datetime not null default current_timestamp
 );
